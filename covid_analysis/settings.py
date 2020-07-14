15d46ed8c18c9c534600bf5ddc15a56ccc6c22bf
Mihir Joshi <mihir67mj@gmail.com>
2020-07-14 11:33:41 +0530

    Updated MySQL Settings

--- covid_analysis/settings.py
+++ covid_analysis/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'covid_analysis.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mysql',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
