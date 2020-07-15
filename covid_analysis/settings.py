4e8c62d54e3866dc86470eb805037dd7e02a6d1b
Mihir Joshi <mihir67mj@gmail.com>
2020-07-15 09:28:54 +0530

    Added Travis CI (#1)
    * Travis setup
    
    * Changed Conda dependency for fbprophet
    
    * Changed Conda dependency for fbprophet
    
    * Changed test script
    
    * Added django install
    
    * Removed test env conda
    
    * pip install django
    
    * added mysqlclient
    
    * added mysql
    
    * removed password from settings.py
    
    * changed python command
    
    * changed packages to pip

--- covid_analysis/settings.py
+++ covid_analysis/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'mysql',
         'USER': 'root',
-        'PASSWORD': 'root',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
