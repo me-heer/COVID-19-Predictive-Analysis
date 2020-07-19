a78b92c7fe10e56665e68538ecd8710f1c0de6f3
Mihir Joshi <mihir67mj@gmail.com>
2020-07-19 15:15:53 +0530

    Tagged Docker Image

--- heroku_push.sh
+++ heroku_push.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 echo "$HEROKU_PASSWORD" | docker login -u "$HEROKU_USERNAME" --password-stdin registry.heroku.com
-docker push mformihir/covid-analysis-mformihir
+docker tag covid-19-predictive-analysis_web $DOCKER_USERNAME/covid-19-predictive-analysis_web:latest
+docker push mformihir/covid-19-predictive-analysis_web:latest
