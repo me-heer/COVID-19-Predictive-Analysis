625e488ffb85dfb556ac08877e52df94d5d4cb61
Mihir Joshi <mihir67mj@gmail.com>
2020-07-19 13:20:20 +0530

    Added django migrate

--- scheduler/update.py
+++ scheduler/update.py
@@ -3,9 +3,10 @@ from apscheduler.schedulers.background import BackgroundScheduler
 import logging
 
 def start():
-    scheduler = BackgroundScheduler()
-    scheduler.add_job(my_logger, 'interval', seconds=3)
-    scheduler.start()
+    logger.warning("WE CAN START SCHEDULER HERE")
+    #scheduler = BackgroundScheduler()
+    #scheduler.add_job(my_logger, 'interval', seconds=3)
+    #scheduler.start()
 
 def my_logger():
     logger = logging.getLogger(__name__)
