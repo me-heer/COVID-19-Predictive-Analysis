46153ddbbf98857285bed5487b84d041055fcc6e
Mihir Joshi <mihir67mj@gmail.com>
2020-07-19 18:42:20 +0530

    Added Heroku Registry

--- heroku_push.sh
+++ heroku_push.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "$HEROKU_PASSWORD" | docker login -u "$HEROKU_USERNAME" --password-stdin registry.heroku.com
-docker tag covid-19-predictive-analysis_web $DOCKER_USERNAME/covid-19-predictive-analysis_web:latest
-docker push mformihir/covid-19-predictive-analysis_web:latest
+docker tag covid-19-predictive-analysis_web registry.heroku.com/covid-analysis-mformihir/web
+docker push registry.heroku.com/covid-analysis-mformihir/web
