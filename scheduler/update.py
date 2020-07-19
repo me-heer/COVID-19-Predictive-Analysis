f64a228d7858128b00865ef9d3ef05eaf8e4e446
Mihir Joshi <mihir67mj@gmail.com>
2020-07-19 13:52:55 +0530

    Fixed update

--- scheduler/update.py
+++ scheduler/update.py
@@ -3,7 +3,6 @@ from apscheduler.schedulers.background import BackgroundScheduler
 import logging
 
 def start():
-    logger.warning("WE CAN START SCHEDULER HERE")
     #scheduler = BackgroundScheduler()
     #scheduler.add_job(my_logger, 'interval', seconds=3)
     #scheduler.start()
