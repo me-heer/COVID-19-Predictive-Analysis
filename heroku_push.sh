875fbb766066bad2bbed137392216722933e357d
Mihir <mihir67mj@gmail.com>
2020-07-20 08:37:56 +0530

    Added heroku deploy
    Added Heroku Key
    
    
    Added django migrate
    
    Fixed update
    
    Fixed update
    
    Not using deploy.sh
    
    Deploy to Heroku Registry
    
    Changed repo name
    
    Tagged Docker Image
    
    Added Heroku Registry
    
    Changed login script
    
    Changed travis config
    
    Changed docker-compose
    
    Fixed
    
    Fixed
    
    Fixed
    
    Fixed
    
    Fixed
    
    Fixed

---
+++ heroku_push.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#echo "$HEROKU_PASSWORD" | docker login -u "$HEROKU_USERNAME" --password-stdin registry.heroku.com
+docker login --username=$HEROKU_USERNAME --password=$HEROKU_PASSWORD registry.heroku.com
+docker tag covid-19-predictive-analysis_web registry.heroku.com/covid-analysis-mformihir/web
+docker push registry.heroku.com/covid-analysis-mformihir/web
--- scheduler/update.py
+++ scheduler/update.py
@@ -4,8 +4,8 @@ import logging
 
 def start():
     scheduler = BackgroundScheduler()
-    scheduler.add_job(my_logger, 'interval', seconds=3)
-    scheduler.start()
+    #scheduler.add_job(my_logger, 'interval', seconds=3)
+    #scheduler.start()
 
 def my_logger():
     logger = logging.getLogger(__name__)
